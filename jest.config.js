b2a8654886490c547883c9f48cf188fa25474c3c
DESKTOP-J4JSEMA\ho <avinash.chauhan@qpaix.com>
2026-05-13 17:13:15 +0530

    fix: restrict jest to src/__tests__ only

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
+  testMatch: ['**/src/__tests__/**/*.test.js'],
   coverageDirectory: 'coverage',
   collectCoverageFrom: ['src/**/*.js', '!src/__tests__/**']
 };
